3cba13e8f395d4a1ba2e51589fcbf50b7f3f1c80
Devaki Nanda Gopal <devakinandagopal@Devakis-MacBook-Air.local>
2025-08-27 00:13:20 +0530

    Header/Footer changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
 export default function App() {
   return (
     <>
-    Hello    
+      <Header />
+      <Footer />
     </>
   );
 }
